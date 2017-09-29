5b1fab2cc1612338dd8ddd1f99260d6693471494
LevanBorchkhadze <32057828+LevanBorchkhadze@users.noreply.github.com>
2017-09-29 15:19:18 +0200

    Finished directors module

--- Director_module.py
+++ Director_module.py
@@ -1,8 +1,9 @@
 import csv
 from collections import Counter
 import numpy as np
+import re
 
-title = input("Please input movie title >")
+director_name_input = input("Please input name of the director >")
 with open('clean_movie_data.csv', 'r',) as f:
     reader = csv.DictReader(f)
     gross = []
@@ -28,35 +29,75 @@ with open('clean_movie_data.csv', 'r',) as f:
         directors.append(row["director_name"])
 
 
-c = Counter(directors)
-# print(c["Gore Verbinski"])
-
 director_ratings = []
 director_gross = []
 director_budget = []
-director_total_actors = []
+director_movies = []
+director_actor_1 = []
+director_actor_1_indexes = []
+director_actor_2 = []
+director_actor_3 = []
+actor_ratings = []
+director_genres = []
+genre_ratings = []
+director_genre_ratings = []
 for i, x in enumerate(directors):
-    if x == title:
-        print(movie_title[i])
-        print("Genres:", genres[i])
-        print("Actors:", actor_1_name[i] + ",", actor_2_name[i] + ",", actor_3_name[i] + ".")
-
+    if x == director_name_input:
         director_ratings.append(imdb_rating[i])
         director_budget.append(budget[i])
         director_gross.append(gross[i])
-        # director_total_actors.append(actor_1_name[i]  + ",", actor_2_name[i] + ",", actor_3_name[i] + ".")
-       
-print(director_ratings)
-print("The average rating for", title, "is", round(np.mean(director_ratings), 2))
-print("The average budget for", title, "is", round(np.mean(director_budget)))
-print("The average gross revenue for", title, "is", round(np.mean(director_gross), 2))
-print("The average profit for", round((np.mean(director_gross) - np.mean(director_budget)), 2))
-    # else:
-    #     print("Name of the director was not found in database")
-    #     break
+        director_movies.append((movie_title[i]))
+        director_actor_1.append(actor_1_name[i])
+        director_actor_2.append(actor_2_name[i])
+        director_actor_3.append(actor_3_name[i])
+        director_genres.append(genres[i])
 
-print(np.corrcoef(num_of_votes, gross)[1][0])
-print(np.corrcoef(num_of_votes, imdb_rating)[1][0])
-print(np.corrcoef(imdb_rating, gross)[1][0])
-print(np.corrcoef(budget, gross)[1][0])
-print(np.corrcoef(budget, num_of_votes)[1][0])
+
+top_3_movies = sorted(zip(director_ratings, director_movies), reverse=True)[:3]
+director_total_actors = director_actor_1 + director_actor_2 + director_actor_3
+
+ac = Counter(director_total_actors)
+top_3_actors = ac.most_common(3)
+
+tot_direct_genres = [item for word in director_genres for item in word.split('|')]
+g = Counter(tot_direct_genres)
+top_3_genre = g.most_common(3)
+
+for i, x in enumerate(directors):
+    if x == director_name_input and actor_1_name[i] == top_3_actors[0][0]:
+        director_actor_1_indexes.append(i)
+
+for i in director_actor_1_indexes:
+    actor_ratings.append(imdb_rating[i])
+
+for i, v in enumerate(genres):
+    if top_3_genre[0][0] in genres:
+        genre_ratings.append(imdb_rating[i])
+
+for i, v in enumerate(genres):
+    if re.search(r'.*'+top_3_genre[0][0]+'.*', v) and director_name_input == directors[i]:
+        director_genre_ratings.append(imdb_rating[i])
+
+
+print("Three most successful movies of", director_name_input, "are:", "\n", top_3_movies[0][1], "-", top_3_movies[0][0], "\n",
+      top_3_movies[1][1], "-", top_3_movies[1][0], "\n",
+      top_3_movies[2][1], "-", top_3_movies[2][0])
+print("Top three co-works:", top_3_actors[0][0], "-", top_3_actors[0][1], "|", top_3_actors[1][0], "-", top_3_actors[1][1], "|",
+      top_3_actors[2][0], "-", top_3_actors[2][1])
+print("Average movie rating for", director_name_input, "and his most frequent co-work", top_3_actors[0][0], "is:", round(np.mean(actor_ratings), 2))
+print("The average rating for", director_name_input, "is", round(np.mean(director_ratings), 2))
+print("The average budget for", director_name_input, "is", round(np.mean(director_budget)))
+print("The average gross revenue for", director_name_input, "is", round(np.mean(director_gross), 2))
+print("The average profit for", round((np.mean(director_gross) - np.mean(director_budget)), 2))
+print("Top 3 most frequent genre for", director_name_input, "is:", "\n",
+      top_3_genre[0][0], "-", top_3_genre[0][1], "|", top_3_genre[1][0],
+      "-", top_3_genre[1][1], "|", top_3_genre[2][0], "-", top_3_genre[2][1])
+print("Average", top_3_genre[0][0]," rating for whole database is", "-", round(np.mean(genre_ratings), 2), "\n",
+      "Average", top_3_genre[0][0], " rating for", director_name_input, "is -", np.mean(director_genre_ratings))
+
+
+print(round(np.corrcoef(num_of_votes, gross)[1][0], 2))
+print(round(np.corrcoef(num_of_votes, imdb_rating)[1][0], 2))
+print(round(np.corrcoef(imdb_rating, gross)[1][0], 2))
+print(round(np.corrcoef(budget, gross)[1][0], 2))
+print(round(np.corrcoef(budget, num_of_votes)[1][0], 2))
